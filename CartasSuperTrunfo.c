2cb16a804a7575b3c1d7a4344eee17605bd13848
codeSpaces <142416127+ssantosGt@users.noreply.github.com>
2025-04-18 18:45:47 +0000

    Finalizei o trabalho por completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,13 @@ int main() {
     char nomecidade[20];
     int populacao;
     float areaemKm;
-    float Pib;
+    double Pib;
     int NpontosT;
     float densidadepop1;
+    float superPoder;
+    float pibpercapta;
+    float valorpercapta;
+    float densidade_invertida;
 
     // Carta 2
     char Estado2[20]; 
@@ -17,9 +21,14 @@ int main() {
     char nomecidade2[20];
     int populacao2;
     float areaemKm2;
-    float Pib2; 
+    double Pib2; 
     int NpontosT2;
     float densidadepop2;
+    float superPoder2;
+    float valorpercapta1;
+    float densidade_invertida2;
+
+    
 
     // Início
     printf("Bem-Vindo Ao Jogo Super Trunfo !!!\n");
@@ -41,14 +50,26 @@ int main() {
     scanf("%f", &areaemKm);  
 
     printf("Digite o PIB: ");
-    scanf("%f", &Pib);
+    scanf("%lf", &Pib);
 
     printf("Números de Pontos Turísticos: ");
     scanf("%i", &NpontosT);
 
-    // Cálculo densidade carta 1
-    densidadepop1 = (float) populacao / areaemKm;
+     //densidade invertida 
+     densidade_invertida = (float)areaemKm / populacao;
+     //Cálculo pib per capta
+     valorpercapta = (float) Pib / populacao;
+     // Cálculo densidade carta 1
+     densidadepop1 = (float) populacao / areaemKm;
 
+      //Soma Super Poder
+
+      superPoder =(float)(populacao + areaemKm + Pib + valorpercapta + densidade_invertida);
+    
+    printf("- Super-Poder: %f\n",superPoder);
+
+
+  /*
     // Resultados carta 1
     printf("Sua Carta foi criada com sucesso!!\n");
     printf("- Letra Inicial: %s\n", Estado);
@@ -56,37 +77,51 @@ int main() {
     printf("- Nome da Cidade: %s\n", nomecidade);
     printf("- Número Habitantes: %i\n", populacao);
     printf("- Área em Km²: %.2f\n", areaemKm);
-    printf("- PIB: %.2f\n", Pib);
+    printf("- PIB: %.2lf\n", Pib);
     printf("- Pontos turísticos: %i\n", NpontosT);
     printf("- Densidade Populacional: %.2f\n", densidadepop1);
+    printf("- Pib per capta: %.2f\n",valorpercapta);
+    printf("- Super-Poder: %.2f\n",superPoder); */
 
     // Segunda carta
     printf("\nSegunda Carta !!!\n");
     printf("Insira Informações \n");
 
-    printf("Uma letra de A a H para Cidade: ");
+    printf("- Uma letra de A a H para Cidade: ");
     scanf(" %s", Estado2);
 
-    printf("Um número de 01 a 04: ");
+    printf("- Um número de 01 a 04: ");
     scanf(" %s", Codigocarta2);
 
-    printf("Nome da cidade: ");
+    printf("- Nome da cidade: ");
     scanf(" %s", nomecidade2);
 
-    printf("Número de habitantes: ");
+    printf("- Número de habitantes: ");
     scanf("%i", &populacao2);
 
-    printf("Número de área em Km²: ");
+    printf("- Número de área em Km²: ");
     scanf("%f", &areaemKm2);  
 
-    printf("Digite o PIB: ");
-    scanf("%f", &Pib2);
+    printf("- Digite o PIB: ");
+    scanf("%lf", &Pib2);
 
-    printf("Números de Pontos Turísticos: ");
+    printf("- Números de Pontos Turísticos: ");
     scanf("%i", &NpontosT2);
 
+    //densidade invertida 
+    densidade_invertida2 = (float)areaemKm2 / populacao2;
+    //Cálculo pib per capta
+    valorpercapta1 = (float) Pib2 / populacao2;
     // Cálculo densidade carta 2
     densidadepop2 = (float) populacao2 / areaemKm2;
+
+     //Soma Super Poder
+     superPoder2 =(float)(populacao2 + areaemKm2 + Pib2 + valorpercapta1 + densidade_invertida2);
+
+     printf("- Super-Poder: %f\n",superPoder2);
+
+
+    /*
 
     // Resultados carta 2
     printf("Sua Segunda Carta foi criada com sucesso!!\n");
@@ -98,6 +133,19 @@ int main() {
     printf("- PIB: %.2f\n", Pib2);
     printf("- Pontos turísticos: %i\n", NpontosT2);
     printf("- Densidade Populacional: %.2f\n", densidadepop2);
+    printf("- Super-Poder: %.2f\n", superPoder2);   */
+
+    //comparação 
+    printf("-  ***** PONTUAÇÃO ***** ");
+    printf("- Número Habitantes: %s\n", (populacao > populacao2) ? "Carta 1 venceu!" : "Carta 2 venceu!");
+    printf("- Aréa Em Km2: %s\n", (areaemKm > areaemKm2)? "Carta 1 venceu" : "Carta 2 venceu");
+    printf("- Pib: %s\n", (Pib > Pib2)? "Carta 1 venceu" : "Carta 2 venceu");
+    printf("-Pontos Turisticos: %s\n", (NpontosT > NpontosT2) ? "Carta 1 venceu" : "Carta 2 venceu!");
+    printf("- Densidade Populacional: %s\n", (densidadepop1 > densidadepop2) ? "Carta 1 venceu" : "Carta 2 venceu!");
+    printf("-Super-poder: %s\n", (superPoder > superPoder2) ? "Carta 1 venceu": "Carta 2 venceu!");
+
+
 
     return 0;
+
 }
